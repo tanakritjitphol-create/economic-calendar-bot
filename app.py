7d8e5b08399904fc43d4ef257c98c71739f6590c
tanakritjitphol-create <tanakrit.jitphol@gmail.com>
2026-05-10 19:35:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import pytz
 
 app = Flask(__name__)
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
-CHAT_IDS = os.environ.get("CHAT_IDS", "").split(",")  # ใส่หลาย Chat ID คั่นด้วย , ค่ะ
-ALPHA_KEY = os.environ.get("ALPHA_KEY", "")
+TELEGRAM_TOKEN = os.environ.get("8648221285:AAGnZC1ujew6seX9wxYz3b-WytN75hss5pM", "")
+CHAT_IDS = os.environ.get("8379040124", "").split(",")  # ใส่หลาย Chat ID คั่นด้วย , ค่ะ
+ALPHA_KEY = os.environ.get("XWJGG2KTQW35QXNM", "")
 
 TZ = pytz.timezone("Asia/Bangkok")
 
